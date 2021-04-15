e222dd91adce8592916a0e11a23f041700d155cd
P2502918 <49189875+P2502918@users.noreply.github.com>
2021-04-15 14:40:57 +0100

    Added testing and class for address management

--- Testing4/tstOrder.cs
+++ Testing4/tstOrder.cs
@@ -1,15 +1,16 @@
 ﻿using System;
+using ClassLibrary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Testing4
 {
     [TestClass]
-    public class clsOrder
+    public class tstOrder
     {
         [TestMethod]
         public void InstanceOK()
         {
-            //create an instance of the class we want to create
+            //create an instance of the class we want to create we want to create
             clsOrder AnOrder = new clsOrder();
             //test to see that it exists
             Assert.IsNotNull(AnOrder);
