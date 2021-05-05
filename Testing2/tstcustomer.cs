639f6a2e210db97738bc629a47d37e31a8d42613
Mohamed Safurulla Jhan <P2442025@my365.dmu.ac.uk>
2021-05-05 10:58:19 +0100

    Added testing and class for customer management

---
+++ Testing2/tstcustomer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstcustomer
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clscustomer Ancustomer = new clscustomer();
+            Assert.IsNotNull(Ancustomer);
+        }
+    }
+}
